bf63ccb714ea4c4ce2957583f2813dabead76e54
saniayudin <olex.yudin@gmail.com>
2022-12-28 20:09:26 +0200

    init commit

---
+++ src/main/java/com/example/geragev2/GerageV2Application.java
@@ -0,0 +1,13 @@
+package com.example.geragev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerageV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerageV2Application.class, args);
+    }
+
+}
